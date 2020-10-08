5615194f2e2c0ce38df9e2d8bbe79fdd85f4a18d
Irina March <marchankairyna@gmail.com>
2020-10-08 17:06:25 +0300

    first

--- Onliner/PageObjects/ItemPage.cs
+++ Onliner/PageObjects/ItemPage.cs
@@ -6,13 +6,13 @@ namespace Onliner.PageObjects
     public class ItemPage
     {
         [FindsBy(How = How.XPath, Using = "//div[contains(@class,'item--highlighted state')]//a[contains(@class,'button_orange')]")] [CacheLookup]
-        private IWebElement _shopChoise;
+        private IWebElement _shopChoice;
 
         public void AddInBasket()
         {
-            if (_shopChoise.Text == "В корзину")
+            if (_shopChoice.Text == "В корзину")
             {
-                _shopChoise.Click();
+                _shopChoice.Click();
             }
         }
     }
