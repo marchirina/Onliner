d39684dc33c35cbe018f2ceb7331e496d0538224
Irina March <marchankairyna@gmail.com>
2020-10-08 22:03:30 +0300

    new changes

--- Onliner/TestCases/Authorization/SingIn.cs
+++ Onliner/TestCases/Authorization/SingIn.cs
@@ -6,7 +6,7 @@ namespace Onliner.TestCases
     public class SingIn : BaseTest
     {
         [Test]
-        public void LogIn()
+        public void CheckChatItem()
         {
             Pages.Main.IsVisibleChatItem();
         }
