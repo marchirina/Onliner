d875c53fc5f6c6f80826f6aaaa32f3868e6c0bae
Irina March <marchankairyna@gmail.com>
2020-10-17 14:09:48 +0300

    report portal

--- Onliner/Properties/AssemblyInfo.cs
+++ Onliner/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Onliner")]
@@ -10,6 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 [assembly: ComVisible(false)]
 
